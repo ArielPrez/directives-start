89ec6d8612271223258d8421bdb7171291e6c294
Ariel Perez <ariel@Ariels-MacBook-Pro.local>
2020-09-11 06:22:06 -0400

    trying ngClass and ngStyle

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,5 +10,14 @@ export class AppComponent {
   // numbers = [1, 2, 3, 4, 5];
   oddNum = [1, 3, 5];
   evenNum = [2, 4];
-  onlyOdd = false;
+  onlyOdd = true;
+  typeNum = 'Even';
+  changeWord(){
+    this.onlyOdd = !this.onlyOdd;
+    if(!this.onlyOdd){
+      this.typeNum = 'Odd';
+    }else{
+      this.typeNum = 'Even';
+    }
+  }
 }
