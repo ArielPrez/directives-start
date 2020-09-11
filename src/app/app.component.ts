f393b6aa92cb8fcad0ed05034b23df2980368524
Ariel Perez <ariel@Ariels-MacBook-Pro.local>
2020-09-11 05:33:14 -0400

    new project generated

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'directives-start';
+  // numbers = [1, 2, 3, 4, 5];
+  oddNum = [1, 3, 5];
+  evenNum = [2, 4];
+  onlyOdd = false;
 }
