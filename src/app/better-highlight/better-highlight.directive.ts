d052f8ef3b83c782f281c7c2d967e6484f53e33d
Ariel Perez <ariel@Ariels-MacBook-Pro.local>
2020-09-12 23:50:27 -0400

    Better Attribute directive example, with Render2

--- src/app/better-highlight/better-highlight.directive.ts
+++ src/app/better-highlight/better-highlight.directive.ts
@@ -1,16 +1,22 @@
-import { Directive, OnInit, Renderer2, ElementRef } from '@angular/core';
+import { Directive, OnInit, Renderer2, ElementRef, HostListener } from '@angular/core';
 
 @Directive({
   selector: '[appBetterHighlight]'
 })
 export class BetterHighlightDirective implements OnInit {
 
-  constructor(private elRef: ElementRef, private render: Renderer2) { }
+  constructor(private elRef: ElementRef, private renderer: Renderer2) { }
 
   ngOnInit(): void {
     // Called after the constructor,
     // initializing input properties, and the first call to ngOnChanges.
-    this.render.setStyle(this.elRef.nativeElement, 'background-color', 'blue');
+    // this.render.setStyle(this.elRef.nativeElement, 'background-color', 'blue', false, false);
   }
 
+  @HostListener('mouseenter') mouseenter(eventData: Event){
+    this.renderer.setStyle(this.elRef.nativeElement, 'background-color', 'lightblue');
+  }
+  @HostListener('mouseleave') mouseleave(eventData: Event){
+    this.renderer.setStyle(this.elRef.nativeElement, 'background-color', 'transparent');
+  }
 }
